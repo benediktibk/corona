7e5f98c36869e3928545194ad6a97bad8810d205
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-01 08:31:12 +0200

    Replace workaround for line width again

--- Corona/ScalableVectorGraphic/XYGraph.cs
+++ Corona/ScalableVectorGraphic/XYGraph.cs
@@ -35,7 +35,7 @@ namespace ScalableVectorGraphic
 
             var xAxisTransformation = xAxis.CreateAxisTransformation(dataSeriesRange.MinimumX, dataSeriesRange.MaximumX);
             var yAxisTransformation = yAxis.CreateAxisTransformation(dataSeriesRange.MinimumY, dataSeriesRange.MaximumY);
-            var overallScalingFactor = Math.Sqrt(Math.Abs(xAxisTransformation.ScalingFactor) * Math.Abs(yAxisTransformation.ScalingFactor))*100;
+            var overallScalingFactor = Math.Sqrt(Math.Abs(xAxisTransformation.ScalingFactor) * Math.Abs(yAxisTransformation.ScalingFactor));
 
             foreach (var dataSeries in allDataSeries) {
                 elements.AddRange(dataSeries.CreateGraphicElements(xAxis.NumericOperations, yAxis.NumericOperations, xAxisTransformation, yAxisTransformation, overallScalingFactor));
