65d01e184c4862c8df67e5380df15bb44e5bd066
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-01 08:43:33 +0200

    Derive Logarithmic Axis from AxisBase

--- Corona/ScalableVectorGraphic/LogarithmicAxis.cs
+++ Corona/ScalableVectorGraphic/LogarithmicAxis.cs
@@ -1,57 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ScalableVectorGraphic
+﻿namespace ScalableVectorGraphic
 {
-    public class LogarithmicAxis<T> : IAxis<T>
+    public class LogarithmicAxis<T> : AxisBase<T>
     {
-        private const double _axisWidth = 0.002;
-        private const double _tickMarkLength = 0.01;
-        private const double _tickMarkWidth = 0.001;
-        private const string _labelFont = "monospace";
-        private const double _fontSize = 0.02;
-
-        public LogarithmicAxis(IGenericNumericOperations<T> numericOperations) {
-            NumericOperations = numericOperations;
+        public LogarithmicAxis(IGenericNumericOperations<T> numericOperations) :
+            base(numericOperations) {
         }
-
-        public IGenericNumericOperations<T> NumericOperations { get; }
-
-        public IAxisTransformation CreateAxisTransformation(double minimumValue, double maximumValue) {
+        
+        public override IAxisTransformation CreateAxisTransformation(double minimumValue, double maximumValue) {
             return new LogarithmicAxisTransformation(minimumValue, maximumValue);
-        }
-
-        public List<IGraphicElement> CreateGraphicElementsForHorizontalAxis(double minimumValue, double maximumValue) {
-            var result = new List<IGraphicElement>();
-            result.Add(new Line("horizontal axis", new Point(0, 0), new Point(1, 0), Color.Black, _axisWidth));
-            var axisTransformation = CreateAxisTransformation(minimumValue, maximumValue);
-
-            for (var i = axisTransformation.AxisStartValue * 10; i < axisTransformation.AxisEndValue; i = i * 10) {
-                double position = axisTransformation.Apply(i);
-                result.Add(new Line("horizontal axis tick mark", new Point(position, 0), new Point(position, _tickMarkLength), Color.Black, _tickMarkWidth));
-                var label = NumericOperations.CreateLabel(i);
-                var halfLabelLength = label.Length / 2.0;
-                var labelOffsetFromTick = halfLabelLength * _fontSize * (-0.5);
-                result.Add(new Text("horizontal axis tick label", new Point(position + labelOffsetFromTick, (-1.1) * _fontSize), label, Color.Black, 0, _labelFont, _fontSize, "middle", "middle"));
-            }
-
-            return result;
-        }
-
-        public List<IGraphicElement> CreateGraphicElementsForVerticalAxis(double minimumValue, double maximumValue) {
-            var result = new List<IGraphicElement>();
-            result.Add(new Line("vertical axis", new Point(0, 0), new Point(0, 1), Color.Black, _axisWidth));
-            var axisTransformation = CreateAxisTransformation(minimumValue, maximumValue);
-
-            for (var i = axisTransformation.AxisStartValue * 10; i < axisTransformation.AxisEndValue; i = i * 10) {
-                double position = axisTransformation.Apply(i);
-                result.Add(new Line("vertical axis tick mark", new Point(0, position), new Point(_tickMarkLength, position), Color.Black, _tickMarkWidth));
-                var label = NumericOperations.CreateLabel(i);
-                var labelOffsetFromTick = label.Length * _fontSize * (-1);
-                result.Add(new Text("vertical axis tick label", new Point(labelOffsetFromTick, position - _fontSize/2), label, Color.Black, 0, _labelFont, _fontSize, "middle", "middle"));
-            }
-
-            return result;
         }
     }
 }
