8c325f4a0923722b89c7d4e49e5ba61948a58531
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-01 15:21:19 +0200

    Add SelectionController

---
+++ Corona/CoronaSpreadViewer/Controllers/SelectionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CoronaSpreadViewer.Controllers
+{
+    public class SelectionController : ApiController
+    {
+        public SelectionController() {
+        }
+
+        [HttpPost]
+        [Route("api/selection/apply")]
+        public HttpResponseMessage Apply(string[] selectedCountries) {
+            var response = Request.CreateResponse(HttpStatusCode.Moved);
+            var rootUri = Request.RequestUri.GetLeftPart(UriPartial.Authority);
+            response.Headers.Location = new Uri(rootUri);
+            return response;
+        }
+    }
+}
