94cd63ef83782a627b5b7d1151c1c0af4e8fac71
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-02 12:06:23 +0200

    Improve Logging

--- Corona/Backend/Service/AuthorizationService.cs
+++ Corona/Backend/Service/AuthorizationService.cs
@@ -1,10 +1,12 @@
-﻿using System.DirectoryServices.AccountManagement;
+﻿using NLog;
+using System.DirectoryServices.AccountManagement;
 using System.Linq;
 
 namespace Backend.Service
 {
     public class AuthorizationService : IAuthorizationService
     {
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
         private readonly ISettings _settings;
 
         public AuthorizationService(ISettings settings) {
@@ -14,11 +16,18 @@ namespace Backend.Service
         public bool IsAllowedToUpdateData(string username, string password) {
             using (var context = new PrincipalContext(ContextType.Domain)) {
                 if (!context.ValidateCredentials(username, password)) {
+                    _logger.Info($"password for user {username} is incorrect");
                     return false;
                 }
             }
 
-            return _settings.AdminUsers.Contains(username);
+            var result = _settings.AdminUsers.Contains(username);
+
+            if (!result) {
+                _logger.Info($"user {username} is not allowed to execute the update");
+            }
+
+            return result;
         }
     }
 }
--- Corona/CoronaSpreadViewer/Controllers/DataController.cs
+++ Corona/CoronaSpreadViewer/Controllers/DataController.cs
@@ -1,5 +1,6 @@
 ﻿using Backend;
 using Backend.Service;
+using NLog;
 using System;
 using System.Net;
 using System.Net.Http;
@@ -9,6 +10,7 @@ namespace CoronaSpreadViewer.Controllers
 {
     public class DataController : ApiController
     {
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
         private readonly IDataReimportService _dataReimportService;
         private readonly IAuthorizationService _authorizationService;
         private readonly IUnitOfWorkFactory _unitOfWorkFactory;
@@ -22,15 +24,22 @@ namespace CoronaSpreadViewer.Controllers
         [HttpPost]
         [Route("api/data/reimport")]
         public HttpResponseMessage Reimport(Credentials credentials) {
+            _logger.Info($"user {credentials.Username} tries to update the data");
+
             if (!_authorizationService.IsAllowedToUpdateData(credentials.Username, credentials.Password)) {
+                _logger.Info($"user {credentials.Username} is not allowed to update the data");
                 return new HttpResponseMessage(HttpStatusCode.Unauthorized);
             }
+
+            _logger.Info("triggering update of data");
 
             using (var unitOfWork = _unitOfWorkFactory.Create()) {
                 unitOfWork.BeginDatabaseTransaction();
                 _dataReimportService.ReimportAll(unitOfWork);
                 unitOfWork.CommitDatabaseTransaction();
             }
+
+            _logger.Info("successfully updated data, redirecting to start page");
 
             var response = Request.CreateResponse(HttpStatusCode.Moved);
             var rootUri = Request.RequestUri.GetLeftPart(UriPartial.Authority);
