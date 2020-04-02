b44710aa939c24272d9f33da9fef6c3c10b8d733
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-02 17:20:08 +0200

    Improve predefined colors

--- Corona/Backend/PredefinedColors.cs
+++ Corona/Backend/PredefinedColors.cs
@@ -14,12 +14,12 @@ namespace Backend
                 new Color(179, 119, 0),
                 new Color(0, 128, 21),
                 new Color(0, 21, 128),
-                new Color(68, 0, 102),
                 new Color(102, 102, 0),
                 new Color(0, 128, 128),
                 new Color(255, 85, 0),
                 new Color(0, 204, 0),
                 new Color(106, 77, 255),
+                new Color(68, 0, 102),
             };
         }
 
