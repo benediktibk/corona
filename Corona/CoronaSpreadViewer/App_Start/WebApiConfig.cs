14bc08215e618e601c80b9c7b31ce4f7c54370b1
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-03 18:58:59 +0200

    Implement Gzip Compression

--- Corona/CoronaSpreadViewer/App_Start/WebApiConfig.cs
+++ Corona/CoronaSpreadViewer/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http;
+﻿using Microsoft.AspNet.WebApi.Extensions.Compression.Server;
+using System.Net.Http.Extensions.Compression.Core.Compressors;
+using System.Web.Http;
 using System.Web.Http.Dispatcher;
 
 namespace CoronaSpreadViewer
@@ -8,6 +10,7 @@ namespace CoronaSpreadViewer
         public static void Register(HttpConfiguration config) {
             config.Services.Replace(typeof(IHttpControllerActivator), new App_Start.ServiceActivator(config));
             config.MapHttpAttributeRoutes();
+            config.MessageHandlers.Insert(0, new ServerCompressionHandler(new GZipCompressor(), new DeflateCompressor()));
         }
     }
 }
