b77ee7aa97cdcb3c67596ad4bfab7c967053b394
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-04 10:47:56 +0200

    Add empty hull for custom server side cache

---
+++ Corona/CoronaSpreadViewer/ServerSideCache.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using WebApi.OutputCache.Core.Cache;
+
+namespace CoronaSpreadViewer
+{
+    public class ServerSideCache : IApiOutputCache
+    {
+        public IEnumerable<string> AllKeys => throw new NotImplementedException();
+
+        public void Add(string key, object o, DateTimeOffset expiration, string dependsOnKey = null) {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(string key) {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(string key) where T : class {
+            throw new NotImplementedException();
+        }
+
+        public object Get(string key) {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key) {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveStartsWith(string key) {
+            throw new NotImplementedException();
+        }
+    }
+}
