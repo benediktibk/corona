94669499d793b19b897a685194da5fe9074ec5f5
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-06 15:34:23 +0200

    Add Test for Gradient Method

--- Corona/MathTest/GradientMethodMinimizationTest.cs
+++ Corona/MathTest/GradientMethodMinimizationTest.cs
@@ -1,6 +1,8 @@
 ﻿using FluentAssertions;
 using Math;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
 namespace MathTest
 {
     [TestClass]
@@ -50,6 +52,25 @@ namespace MathTest
             distance.Should().BeGreaterThan(100);
             result.X.Should().NotBeApproximately(penaltyPoint.X, 1e-5);
             result.Y.Should().NotBeApproximately(penaltyPoint.Y, 1e-5);
+        }
+
+        [TestMethod]
+        public void Minimize_Box_CenterOfBox() {
+            var bottom = new LineExponentialDistancePenaltyFunction(new Vector(0, 0), new Vector(1, 0), 5, 1e10);
+            var top = new LineExponentialDistancePenaltyFunction(new Vector(0, 5), new Vector(1, 0), 5, 1e10);
+            var left = new LineExponentialDistancePenaltyFunction(new Vector(0, 0), new Vector(0, 1), 5, 1e10);
+            var right = new LineExponentialDistancePenaltyFunction(new Vector(10, 0), new Vector(0, 1), 5, 1e10);
+            var penaltyFunction = new PenaltyFunctionSum(new List<IPenaltyFunction> {
+                bottom,
+                top,
+                left,
+                right
+            });
+
+            var result = GradientMethodMinimization.Minimize(new Vector(0.01, 0.1), penaltyFunction, 10, 1e-5);
+
+            result.X.Should().NotBeApproximately(2.5, 1e-5);
+            result.Y.Should().NotBeApproximately(5, 1e-5);
         }
     }
 }
