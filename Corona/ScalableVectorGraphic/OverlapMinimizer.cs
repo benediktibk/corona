b9ee86178c61b612ec52233fc434ead0212074ba
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-06 22:10:24 +0200

    Tweak paramter for OverlapMinimizer

--- Corona/ScalableVectorGraphic/OverlapMinimizer.cs
+++ Corona/ScalableVectorGraphic/OverlapMinimizer.cs
@@ -6,9 +6,10 @@ namespace ScalableVectorGraphic
     {
         public static Point PlaceRectangleOverPoints(Rectangle rectangle, IReadOnlyList<Point> points) {
             const double weight = 1;
-            const double springDistanceBorder = 0.3;
+            const double springDistanceBorder = 0.1;
             const double springStiffness = 1;
-            const double springLength = 1;
+            const double springLengthBorder = 1;
+            const double springLengthPoints = 0.1;
             const double runTime = 10;
             const double positionEpsilon = 1e-5;
             const double timeStep = 1e-2;
@@ -17,31 +18,31 @@ namespace ScalableVectorGraphic
 
             for (var x = springDistanceBorder; x < 1; x += springDistanceBorder) {
                 var connectionPoint = new Math.FixedPoint(new Math.Vector(x, 0));
-                var spring = new Math.Spring(springLength, springStiffness, connectionPoint, rectangleWithSprings);
+                var spring = new Math.Spring(springLengthBorder, springStiffness, connectionPoint, rectangleWithSprings);
                 rectangleWithSprings.AddSpringBottom(spring);
             }
 
             for (var x = springDistanceBorder; x < 1; x += springDistanceBorder) {
                 var connectionPoint = new Math.FixedPoint(new Math.Vector(x, 1));
-                var spring = new Math.Spring(springLength, springStiffness, connectionPoint, rectangleWithSprings);
+                var spring = new Math.Spring(springLengthBorder, springStiffness, connectionPoint, rectangleWithSprings);
                 rectangleWithSprings.AddSpringTop(spring);
             }
 
             for (var y = springDistanceBorder; y < 1; y += springDistanceBorder) {
                 var connectionPoint = new Math.FixedPoint(new Math.Vector(0, y));
-                var spring = new Math.Spring(springLength, springStiffness, connectionPoint, rectangleWithSprings);
+                var spring = new Math.Spring(springLengthBorder, springStiffness, connectionPoint, rectangleWithSprings);
                 rectangleWithSprings.AddSpringLeft(spring);
             }
 
             for (var y = springDistanceBorder; y < 1; y += springDistanceBorder) {
                 var connectionPoint = new Math.FixedPoint(new Math.Vector(1, y));
-                var spring = new Math.Spring(springLength, springStiffness, connectionPoint, rectangleWithSprings);
+                var spring = new Math.Spring(springLengthBorder, springStiffness, connectionPoint, rectangleWithSprings);
                 rectangleWithSprings.AddSpringRight(spring);
             }
 
             foreach (var point in points) {
                 var connectionPoint = new Math.FixedPoint(point.ToVector());
-                var spring = new Math.Spring(springLength, springStiffness, connectionPoint, rectangleWithSprings);
+                var spring = new Math.Spring(springLengthPoints, springStiffness, connectionPoint, rectangleWithSprings);
                 rectangleWithSprings.AddSpringCenter(spring);
             }
 
