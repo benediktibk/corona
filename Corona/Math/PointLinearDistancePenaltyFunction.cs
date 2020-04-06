8bcde4998396d69c4c79e965d0ce4109d004238a
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-06 19:00:04 +0200

    Implement point with linear distance behaviour

---
+++ Corona/Math/PointLinearDistancePenaltyFunction.cs
@@ -0,0 +1,40 @@
+﻿namespace Math
+{
+    public class PointLinearDistancePenaltyFunction : IPenaltyFunctionIntegrable
+    {
+        private readonly double _gradient;
+        private readonly double _maximumValue;
+        private readonly Vector _position;
+
+        public PointLinearDistancePenaltyFunction(Vector position, double gradient, double maximumValue) {
+            _position = position;
+            _gradient = gradient;
+            _maximumValue = maximumValue;
+        }
+
+        public Vector CalculateGradient(Vector position) {
+            var distance = CalculateDistance(position);
+
+            if (distance == 0) {
+                return new Vector(0, 0);
+            }
+
+            var completeFactor =  (-1) * _gradient / distance;
+            return completeFactor * (position - _position);
+        }
+
+        public double CalculateValue(Vector position) {
+            var distance = CalculateDistance(position);
+            return System.Math.Max(0, _maximumValue - _gradient * distance);
+        }
+
+        private double CalculateDistance(Vector position) {
+            var distanceVector = position - _position;
+            return distanceVector.Norm;
+        }
+
+        public double CalculateValueSumInRectangle(Vector position, double width, double height) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Corona/MathTest/PointLinearDistancePenaltyFunctionTest.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathTest
+{
+    [TestClass]
+    public class PointLinearDistancePenaltyFunctionTest
+    {
+        [TestMethod]
+        public void CalculateValue_AtOwnPosition_MaximumValue() {
+            var penaltyFunction = new PointLinearDistancePenaltyFunction(new Vector(3, 6), 4, 10);
+
+            var result = penaltyFunction.CalculateValue(new Vector(3, 6));
+
+            result.Should().BeApproximately(10, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_AtOwnPosition_0() {
+            var penaltyFunction = new PointLinearDistancePenaltyFunction(new Vector(3, 6), 4, 10);
+
+            var result = penaltyFunction.CalculateGradient(new Vector(3, 6));
+
+            result.Norm.Should().BeApproximately(0, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateValue_Nearby_CorrectValue() {
+            var penaltyFunction = new PointLinearDistancePenaltyFunction(new Vector(3, 6), 4, 10);
+
+            var result = penaltyFunction.CalculateValue(new Vector(3, 7));
+
+            result.Should().BeApproximately(6, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_NearbyInY_CorrectValue() {
+            var penaltyFunction = new PointLinearDistancePenaltyFunction(new Vector(3, 6), 4, 10);
+
+            var result = penaltyFunction.CalculateGradient(new Vector(3, 7));
+
+            result.X.Should().BeApproximately(0, 1e-5);
+            result.Y.Should().BeApproximately(-4, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_NearbyInX_CorrectValue() {
+            var penaltyFunction = new PointLinearDistancePenaltyFunction(new Vector(3, 6), 4, 10);
+
+            var result = penaltyFunction.CalculateGradient(new Vector(2, 6));
+
+            result.X.Should().BeApproximately(4, 1e-5);
+            result.Y.Should().BeApproximately(0, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateValue_Farway_0() {
+            var penaltyFunction = new PointLinearDistancePenaltyFunction(new Vector(3, 6), 4, 10);
+
+            var result = penaltyFunction.CalculateValue(new Vector(30, 700));
+
+            result.Should().BeApproximately(0, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_Farway_CorrectDirection() {
+            var penaltyFunction = new PointLinearDistancePenaltyFunction(new Vector(3, 6), 4, 10);
+
+            var result = penaltyFunction.CalculateGradient(new Vector(3, 700));
+
+            result.X.Should().BeApproximately(0, 1e-5);
+            result.Y.Should().BeApproximately(-4, 1e-5);
+        }
+    }
+}
