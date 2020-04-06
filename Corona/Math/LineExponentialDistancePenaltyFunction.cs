c7b31e79e6c87c69f085872f8effa6899514a2a4
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-06 15:30:32 +0200

    Implement Line Penalty Function

--- Corona/Math/LineExponentialDistancePenaltyFunction.cs
+++ Corona/Math/LineExponentialDistancePenaltyFunction.cs
@@ -6,20 +6,32 @@
         private readonly Vector _direction;
         private readonly double _exponentialBase;
         private readonly double _maximumValue;
+        private readonly double _distanceOffset;
 
         public LineExponentialDistancePenaltyFunction(Vector offset, Vector direction, double exponentialBase, double maximumValue) {
             _offset = offset;
             _direction = 1/ direction.Norm * direction;
             _exponentialBase = exponentialBase;
             _maximumValue = maximumValue;
+            _distanceOffset = System.Math.Log(_maximumValue) / System.Math.Log(_exponentialBase);
         }
 
         public Vector CalculateGradient(Vector position) {
-            throw new System.NotImplementedException();
+            var distanceVector = CalculateDistance(position);
+            var distance = distanceVector.Norm;
+            var logBase = System.Math.Log(_exponentialBase);
+            var value = System.Math.Pow(_exponentialBase, _distanceOffset - distanceVector.Norm);
+            var completeFactor = value * logBase / distance * (-1);
+            return completeFactor * distanceVector;
         }
 
         public double CalculateValue(Vector position) {
-            throw new System.NotImplementedException();
+            var distance = CalculateDistance(position);
+            return System.Math.Pow(_exponentialBase, _distanceOffset - distance.Norm);
+        }
+
+        private Vector CalculateDistance(Vector position) {
+            return (position - _offset) - ((position - _offset) * _direction) * _direction;
         }
 
         public double CalculateValueSumInRectangle(Vector position, double width, double height) {
---
+++ Corona/MathTest/LineExponentialDistancePenaltyFunctionTest.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathTest
+{
+    [TestClass]
+    public class LineExponentialDistancePenaltyFunctionTest
+    {
+        [TestMethod]
+        public void CalculateValue_HorizontalLineThroughPointComparison_SameValueAsPoint() {
+            var point = new PointExponentialDistancePenaltyFunction(new Vector(4, 5), 3, 10);
+            var line = new LineExponentialDistancePenaltyFunction(new Vector(3, 5), new Vector(10, 0), 3, 10);
+
+            var result = line.CalculateValue(new Vector(4, 2));
+
+            var resultShouldBe = point.CalculateValue(new Vector(4, 2));
+            result.Should().BeApproximately(resultShouldBe, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateValue_VerticalLineThroughPointComparison_SameValueAsPoint() {
+            var point = new PointExponentialDistancePenaltyFunction(new Vector(4, 5), 3, 10);
+            var line = new LineExponentialDistancePenaltyFunction(new Vector(4, -5), new Vector(0, 2), 3, 10);
+
+            var result = line.CalculateValue(new Vector(2, 5));
+
+            var resultShouldBe = point.CalculateValue(new Vector(2, 5));
+            result.Should().BeApproximately(resultShouldBe, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_HorizontalLineThroughPointComparison_SameValueAsPoint() {
+            var point = new PointExponentialDistancePenaltyFunction(new Vector(4, 5), 3, 10);
+            var line = new LineExponentialDistancePenaltyFunction(new Vector(3, 5), new Vector(10, 0), 3, 10);
+
+            var result = line.CalculateGradient(new Vector(4, 2));
+
+            var resultShouldBe = point.CalculateGradient(new Vector(4, 2));
+            result.X.Should().BeApproximately(resultShouldBe.X, 1e-5);
+            result.Y.Should().BeApproximately(resultShouldBe.Y, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_VerticalLineThroughPointComparison_SameValueAsPoint() {
+            var point = new PointExponentialDistancePenaltyFunction(new Vector(4, 5), 3, 10);
+            var line = new LineExponentialDistancePenaltyFunction(new Vector(4, -5), new Vector(0, 2), 3, 10);
+
+            var result = line.CalculateGradient(new Vector(2, 5));
+
+            var resultShouldBe = point.CalculateGradient(new Vector(2, 5));
+            result.X.Should().BeApproximately(resultShouldBe.X, 1e-5);
+            result.Y.Should().BeApproximately(resultShouldBe.Y, 1e-5);
+        }
+    }
+}
