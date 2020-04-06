60438143d778f1b939de334ae23db51a1acd474f
Benedikt Schmidt <benediktibk@outlook.com>
2020-04-06 19:17:34 +0200

    Implement LineLinear Penalty Function

---
+++ Corona/Math/LineLinearDistancePenaltyFunction.cs
@@ -0,0 +1,71 @@
+﻿namespace Math
+{
+    public class LineLinearDistancePenaltyFunction : IPenaltyFunctionIntegrable
+    {
+        private readonly Vector _offset;
+        private readonly Vector _direction;
+        private readonly double _gradient;
+        private readonly double _maximumValue;
+        private readonly bool _rightSideMaximumValue;
+        private readonly bool _leftSideMaximumValue;
+
+        public LineLinearDistancePenaltyFunction(Vector offset, Vector direction, double gradient, double maximumValue, bool leftSideMaximumValue, bool rightSideMaximumValue) {
+            _offset = offset;
+            _direction = 1 / direction.Norm * direction;
+            _gradient = gradient;
+            _maximumValue = maximumValue;
+            _leftSideMaximumValue = leftSideMaximumValue;
+            _rightSideMaximumValue = rightSideMaximumValue;
+        }
+
+        public Vector CalculateGradient(Vector position) {
+            var distanceVector = CalculateDistance(position);
+
+            if (_rightSideMaximumValue || _leftSideMaximumValue) {
+                var isLeft = Vector.IsLeftOfLine(_offset, _direction, position);
+
+                if (isLeft && _leftSideMaximumValue) {
+                    return distanceVector;
+                }
+
+                if (!isLeft && _rightSideMaximumValue) {
+                    return distanceVector;
+                }
+            }
+
+            var distance = distanceVector.Norm;
+
+            if (distance == 0) {
+                return new Vector(0, 0);
+            }
+
+            var completeFactor = (-1) * _gradient / distance;
+            return completeFactor * distanceVector;
+        }
+
+        public double CalculateValue(Vector position) {
+            if (_rightSideMaximumValue || _leftSideMaximumValue) {
+                var isLeft = Vector.IsLeftOfLine(_offset, _direction, position);
+
+                if (isLeft && _leftSideMaximumValue) {
+                    return _maximumValue;
+                }
+
+                if (!isLeft && _rightSideMaximumValue) {
+                    return _maximumValue;
+                }
+            }
+
+            var distance = CalculateDistance(position);
+            return System.Math.Max(0, _maximumValue - _gradient * distance.Norm);
+        }
+
+        private Vector CalculateDistance(Vector position) {
+            return (position - _offset) - ((position - _offset) * _direction) * _direction;
+        }
+
+        public double CalculateValueSumInRectangle(Vector position, double width, double height) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Corona/MathTest/LineLinearDistancePenaltyFunctionTest.cs
@@ -0,0 +1,142 @@
+﻿using FluentAssertions;
+using Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathTest
+{
+    [TestClass]
+    public class LineLinearDistancePenaltyFunctionTest
+    {
+        [TestMethod]
+        public void CalculateValue_AtLine_MaximumValue() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, false);
+
+            var result = penaltyFunction.CalculateValue(new Vector(5, 3));
+
+            result.Should().BeApproximately(10, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateValue_LeftOfLine_CorrectValue() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, false);
+
+            var result = penaltyFunction.CalculateValue(new Vector(4, 3));
+
+            result.Should().BeApproximately(6, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateValue_RightOfLine_CorrectValue() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, false);
+
+            var result = penaltyFunction.CalculateValue(new Vector(6, 3));
+
+            result.Should().BeApproximately(6, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateValue_LeftFarAwayOfLine_0() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, false);
+
+            var result = penaltyFunction.CalculateValue(new Vector(-999, 3));
+
+            result.Should().BeApproximately(0, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateValue_RightFarAwayOfLine_0() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, false);
+
+            var result = penaltyFunction.CalculateValue(new Vector(699, 3));
+
+            result.Should().BeApproximately(0, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateValue_LeftFarAwayOfLineWithMaximumValueLeft_MaximumValue() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, true, false);
+
+            var result = penaltyFunction.CalculateValue(new Vector(-999, 3));
+
+            result.Should().BeApproximately(10, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateValue_RightFarAwayOfLineWithMaximumValueRight_MaximumValue() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, true);
+
+            var result = penaltyFunction.CalculateValue(new Vector(699, 3));
+
+            result.Should().BeApproximately(10, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_AtLine_0() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, false);
+
+            var result = penaltyFunction.CalculateGradient(new Vector(5, 3));
+
+            result.Norm.Should().BeApproximately(0, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_LeftOfLine_CorrectValue() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, false);
+
+            var result = penaltyFunction.CalculateGradient(new Vector(4, 3));
+
+            result.X.Should().BeApproximately(4, 1e-5);
+            result.Y.Should().BeApproximately(0, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_RightOfLine_CorrectValue() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, false);
+
+            var result = penaltyFunction.CalculateGradient(new Vector(6, 3));
+
+            result.X.Should().BeApproximately(-4, 1e-5);
+            result.Y.Should().BeApproximately(0, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_LeftFarAwayOfLine_CorrectValue() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, false);
+
+            var result = penaltyFunction.CalculateGradient(new Vector(-999, 3));
+
+            result.X.Should().BeApproximately(4, 1e-5);
+            result.Y.Should().BeApproximately(0, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculatGradient_RightFarAwayOfLine_CorrectValue() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, false);
+
+            var result = penaltyFunction.CalculateGradient(new Vector(699, 3));
+
+            result.X.Should().BeApproximately(-4, 1e-5);
+            result.Y.Should().BeApproximately(0, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_LeftFarAwayOfLineWithMaximumValueLeft_CorrectValue() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, true, false);
+
+            var result = penaltyFunction.CalculateGradient(new Vector(-999, 3));
+
+            result.X.Should().BeApproximately(-1004, 1e-5);
+            result.Y.Should().BeApproximately(0, 1e-5);
+        }
+
+        [TestMethod]
+        public void CalculateGradient_RightFarAwayOfLineWithMaximumValueRight_CorrectValue() {
+            var penaltyFunction = new LineLinearDistancePenaltyFunction(new Vector(5, 3), new Vector(0, 1), 4, 10, false, true);
+
+            var result = penaltyFunction.CalculateGradient(new Vector(699, 3));
+
+            result.X.Should().BeApproximately(694, 1e-5);
+            result.Y.Should().BeApproximately(0, 1e-5);
+        }
+    }
+}
