7248c824c5dc7fc79414a66fab9b9c3a3ae3956a
Benedikt Schmidt <benedikt.schmidt@world-direct.at>
2020-08-14 07:22:49 +0200

    Improve Logging

--- Corona/Backend/Repository/InfectionSpreadDataPointRepository.cs
+++ Corona/Backend/Repository/InfectionSpreadDataPointRepository.cs
@@ -22,7 +22,7 @@ namespace Backend.Repository {
         }
 
         public void Insert(IUnitOfWork unitOfWork, InfectionSpreadDataPointDao dataPoint) {
-            _logger.Debug($"adding data point for country {dataPoint.CountryId}");
+            _logger.Trace($"adding data point for country {dataPoint.CountryId}");
             unitOfWork.ExecuteDatabaseCommand(@"
                 INSERT InfectionSpreadDataPoint
                 (
