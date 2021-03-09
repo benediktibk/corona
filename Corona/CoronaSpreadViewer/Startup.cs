2cd753617778597d9b3dcb3ccddb52606a639e1a
Benedikt Schmidt <benediktibk@gmail.com>
2021-03-09 19:36:02 +0100

    use registry for dependency injection

--- Corona/CoronaSpreadViewer/Startup.cs
+++ Corona/CoronaSpreadViewer/Startup.cs
@@ -1,12 +1,19 @@
+using Backend.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StructureMap;
 
 namespace CoronaSpreadViewer {
     public class Startup {
         public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+            var container = new Container();
+            container.Configure(config => {
+                config.AddRegistry(new DependencyInjectionRegistry());
+                config.Populate(services);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
